969b8077a745e977eaee2e561ad98861d24aea9b
Chris Saylor <cjsaylor@gmail.com>
2013-09-26 07:08:35 -0400

    Added some test code.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-var tracer = require('tracer');
 var io = require('socket.io').listen(1337);
+var tracer = require('tracer').colorConsole({
+	level: 'debug'
+});
+
+io.set('logger', {
+	debug: tracer.debug,
+	info: tracer.info,
+	warn: tracer.warn,
+	error: tracer.error
+});
 
 io.sockets.on('connection', function(socket) {
 	socket.emit('handshake', {});
+	setInterval(function() {
+		socket.emit('test_event', {
+			'test': Math.floor(Math.random() * 10001)
+		});
+	}, 3000);
 });
