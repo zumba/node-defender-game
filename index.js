e202ac6fd755276f6a48369844d475ee07ce3aa8
Chris Saylor <christopher.saylor@zumba.com>
2013-09-27 23:48:22 -0400

    Listen for player actions and respond with rounds and "death".

--- index.js
+++ index.js
@@ -27,13 +27,54 @@ io.set('authorization', function(data, accept) {
 defender = io
 	.of('/defender')
 	.on('connection', function(socket) {
-		var player = new Player(socket.handshake.query.username);
+		var player = new Player(socket.handshake.query.username),
+			round = 1;
+
+		// Handle the player's demise
+		player.on('death', function(player) {
+			defender.emit('death', {
+				'message': player.name() + ' is not in my base, killing my dudes anymore.',
+				'stats': player.info()
+			});
+			socket.emit('disconnect', {});
+			
+		});
+
+		// Welcome message
 		socket.emit('handshake', {
 			'message': 'Welcome ' + player.name() + ', prepare to be attacked!'
 		});
-		setInterval(function() {
-			socket.emit('test_event', {
-				'test': Math.floor(Math.random() * 10001)
-			});
-		}, 3000);
+
+		socket.on('action', function(data) {
+			// Recieve action commands from the player
+			// Should emit an event on the game "brain" class.
+			// The game brain should determine how much damage etc per mob per round.
+			var dmg = Math.ceil(Math.random() * 10);
+			round++;
+			player.damage(dmg);
+
+			// Add a delay to not make the game instant
+			setTimeout(function() {
+				socket.emit('round', {
+					player: player.info(),
+					round: round,
+					damage: {
+						taken: dmg,
+						inflicted: 0
+					},
+					mobs: []
+				})
+			}, 1000);
+		});
+
+		// Initial round
+		socket.emit('round', {
+			player: player.info(),
+			round: round,
+			damage: {
+				taken: 0,
+				inflicted: 0
+			},
+			mobs: []
+		});
 	});
