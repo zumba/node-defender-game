8e918385a2aaa22ab53006977d77f47b8cce6478
Chris Saylor <cjsaylor@gmail.com>
2013-10-09 07:49:56 -0400

    Moved client JS from scoreboard to public directory script.

---
+++ public/js/scoreboard.js
@@ -0,0 +1,27 @@
+var socket = io.connect('//localhost:1337/stats');
+socket
+	.on('top10', function (data) {
+		var $toplist;
+		$toplist = $('<ol/>');
+		_.each(data, function(entry) {
+			$toplist.append('<li>' + entry.username + ': ' + entry.round + '</li>');
+		});
+		$('#top10')
+			.html('')
+			.append($toplist);
+	})
+	.on('playerRefresh', function(data) {
+		if (data.length === 0) {
+			$('#playerList')
+				.html('<p>No players :(</p>');
+			return;
+		}
+		var $playerList;
+		$playerList = $('<ul/>');
+		_.each(data, function(player) {
+			$playerList.append('<li>' + player + '</li>');
+		});
+		$('#playerList')
+			.html('')
+			.append($playerList);
+	});
