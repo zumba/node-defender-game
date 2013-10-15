733c3ff0614db4322195b8f63fbf1c402a500256
Chris Saylor <cjsaylor@gmail.com>
2013-10-14 23:41:09 -0400

    Get top 10 by score instead of round.

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -4,7 +4,7 @@ socket
 		var $toplist;
 		$toplist = $('<ol/>');
 		_.each(data, function(entry) {
-			$toplist.append('<li>' + entry.username + ': ' + entry.round + '</li>');
+			$toplist.append('<li>' + entry.username + ': ' + entry.score + '</li>');
 		});
 		$('#top10')
 			.html('')
