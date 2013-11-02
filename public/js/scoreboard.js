b99a163edc6fee09796b2db3197980009e9f507f
Chris Saylor <cjsaylor@gmail.com>
2013-11-02 11:07:46 -0400

    Added underscore template for rendering top10 list.
    The template is for a single entry in the top10 list meant to be
    iterated and rendered multiple times.

--- public/js/scoreboard.js
+++ public/js/scoreboard.js
@@ -1,29 +1,57 @@
-var socket = io.connect(host + '/stats', {
-	secure: secureHost
-});
-socket
-	.on('top10', function (data) {
-		// Defunct atm
-		return;
-		var $toplist;
-		$toplist = $('<ol/>');
-		_.each(data, function(entry) {
-			$toplist.append('<li><span>' + _.escape(entry.username) + '</span> <span>' + Number(entry.score).toLocaleString() + '</span></li>');
-		});
-		$('#top10')
-			.html('')
-			.append($toplist);
-	})
-	.on('playerRefresh', function(data) {
-		var playerList;
-		if (data.length === 0) {
-			$('#playerList')
-				.html('<p>No players :(</p>');
-			return;
-		}
-		playerList = $('<p/>').html(data.join(', '));
-
-		$('#playerList')
-			.html('')
-			.append(playerList);
+$(function() {
+	var socket = io.connect(host + '/stats', {
+		secure: secureHost
 	});
+	var templateCache = {}
+
+	// Retrieve an underscore template object
+	var renderTemplate = function(template, data) {
+		data = data || {};
+		if (templateCache[template]) {
+			return templateCache[template](data);
+		}
+		$.ajax({
+			url: '/templates/' + template + '.html',
+			method: 'GET',
+			async: false,
+			success: function(result) {
+				templateCache[template] = _.template(result);
+			}
+		});
+
+		return templateCache[template](data);
+	}
+
+	socket
+		// top 10 list
+		.on('top10', function (data) {
+			var top10 = $('#top10');
+			top10.find('.leader-row').remove();
+			if (!data) {
+				top10.append('<div class="leader-row"><p>No scores available.</p></div>');
+				return;
+			}
+			_.each(data, function(entry, index) {
+				top10.append(renderTemplate('leader_row', {
+					place: index + 1,
+					image: entry.image,
+					username: _.escape(entry.username),
+					score: Number(entry.score).toLocaleString()
+				}));
+			});
+		})
+		// Current players
+		.on('playerRefresh', function(data) {
+			var playerList;
+			if (data.length === 0) {
+				$('#playerList')
+					.html('<p>No players :(</p>');
+				return;
+			}
+			playerList = $('<p/>').html(data.join(', '));
+
+			$('#playerList')
+				.html('')
+				.append(playerList);
+		});
+})
